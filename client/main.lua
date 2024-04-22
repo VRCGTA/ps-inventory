3705b1f66bb85e7df08684a2121c91bf5f7533a0
azutake <azusa_takemae@dwango.co.jp>
2024-04-22 21:09:53 +0900

    remove waiter

--- client/main.lua
+++ client/main.lua
@@ -461,7 +461,7 @@ RegisterNetEvent('inventory:client:OpenInventory', function(PlayerAmmo, inventor
 
         end)
         else
-            Wait(500)
+            -- Wait(500)
             ToggleHotbar(false)
             if showBlur == true then
                 TriggerScreenblurFadeIn(1000)
